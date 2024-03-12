d4f9d057757ea893c50f9b5970ac594a9afeebf4
Andriy Demchenko <andrii.demchencko.c@pricespider.com>
2024-03-12 14:45:54 +0200

    [HOTFIX] fixed pagination

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from main.create_models_view import create_teachers, create_students, create_groups, create_all_models
+from main.create_models_view import create_teachers, create_students, create_groups, create_all_models, create_problems, \
+    create_lectures
 from main.views import *
 
 urlpatterns = [
@@ -12,7 +13,6 @@ urlpatterns = [
     path('problems/<int:pk>/', ProblemView.as_view(), name='problem'),
     path('problems/<int:pk>/edit/', ProblemUpdateView.as_view(), name='problem_edit'),
     path('problems/<int:pk>/delete/', ProblemDeleteView.as_view(), name='problem_delete'),
-    # path('problems/<int:pk>/edit/', problem_edit, name='problem_edit'),
     path('problems/<int:pk>/take/', ProblemTakeView.as_view(), name='problem_take'),
     path('problems/<int:pk>/solutions/', ProblemSolutionListView.as_view(), name='solutions'),
     path('problems/<int:pk>/solution/', ProblemSolutionView.as_view(), name='solution'),
@@ -33,4 +33,6 @@ urlpatterns = [
     path('create_teachers/', create_teachers, name='create_teachers'),
     path('create_students/', create_students, name='create_students'),
     path('create_groups/', create_groups, name='create_groups'),
+    path('create_problems/', create_problems, name='create_problems'),
+    path('create_lectures/', create_lectures, name='create_lectures'),
 ]
