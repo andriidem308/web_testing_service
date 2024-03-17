441582dce6b220a26ce99eae2cc43a19fba869bc
Andriy Demchenko <andrii.demchencko.c@pricespider.com>
2024-03-18 00:50:00 +0200

    [IN-SITE NOTIFICATIONS] Implemented notification menu: - added model Notification - added a bell to header to highlight received notifications

--- main/context_processors.py
+++ main/context_processors.py
@@ -4,6 +4,12 @@ from main.models import Notification
 def notifications_context(request):
     if request.user.is_authenticated:
         notifications = Notification.objects.filter(user=request.user).order_by('-created_at')
+        unread_notifications = notifications.filter(is_seen=False)
     else:
         notifications = None
-    return {'notifications': notifications}
+        unread_notifications = None
+    context = {
+        'notifications': notifications,
+        'unread_notifications': unread_notifications
+    }
+    return context
