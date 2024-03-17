145096168e2b257a4ae212c18732ce6bc0ffe446
Oleksandr Nikiforov <oleksandr.nikiforov-contractor@procore.com>
2024-03-17 19:50:59 +0200

    Add notification for creating new test and student taking problem

--- main/services/mail_notification.py
+++ main/services/mail_notification.py
@@ -35,3 +35,29 @@ def problem_added_notify(problem):
     subject = f'New problem "{problem_headline}"'
     message = render_to_string('messages/problem_add.html', {'problem_title': problem_headline, 'teacher': teacher})
     send_mail(subject, '', EMAIL_HOST_USER, recipients_email_list, html_message=message)
+
+
+
+# def test_added_notify(test):
+#     teacher = test.teacher
+#     test_headline = test.headline
+#     groups = test.groups.all()
+#
+#     # recipients = Student.objects.filter(group__in=groups)
+#     # recipients_email_list = [recipient.user.email for recipient in recipients]
+#     recipients_email_list = ['nikiforovsh@gmail.com', 'andriidem308@gmail.com']
+#
+#     subject = f'New problem "{test_headline}"'
+#     message = render_to_string('messages/test_add.html', {'test_title': test_headline, 'teacher': teacher})
+#     send_mail(subject, '', EMAIL_HOST_USER, recipients_email_list, html_message=message)
+
+def student_take_problem_notify(solution):
+    student = solution.student
+    problem_headline = solution.problem.headline
+    teacher = solution.problem.teacher
+
+    # teacher_email = teacher.user.email
+    teacher_email = ""
+    subject = f'New solution for problem "{problem_headline}" from {student}'
+    message = render_to_string('messages/student_take_form.html', {'teacher': teacher, 'problem_title': problem_headline, 'student': student})
+    send_mail(subject, '', EMAIL_HOST_USER, [teacher_email], html_message=message)
