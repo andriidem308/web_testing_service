f82645c5d543067fd5c84d69547b5e9efc4e3560
Andriy Demchenko <andrii.demchencko.c@pricespider.com>
2024-03-25 23:16:56 +0200

    main update

--- src/core/settings.py
+++ src/core/settings.py
@@ -141,8 +141,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False').lower() in ('true', '1', 't')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() in ('true', '1', 't')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 # MEDIA FILES
