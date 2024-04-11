22e553a8cc69467714a099963f4059601f19a1f0
Andriy Demchenko <andrii.demchencko.c@pricespider.com>
2024-04-11 23:21:26 +0300

    update

--- src/core/settings.py
+++ src/core/settings.py
@@ -98,7 +98,7 @@ elif os.getenv('DATABASE') == 'postgres':
             'NAME': 'wts',
             'USER': 'wts',
             'PASSWORD': 'wts',
-            'HOST': '134.122.84.248',
+            'HOST': '10.114.0.4',
             'PORT': '',
         }
     }
